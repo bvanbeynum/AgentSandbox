afc83b6c53159d3af6dcaa6d514ab6e73bc22bc1
bvanbeynum <maildrop444@gmail.com>
2026-05-01 14:30:40 -0400

    Move the instructions

--- nodeAgent.js
+++ nodeAgent.js
@@ -1,7 +1,7 @@
 import { BaseAgent } from "./agentCore.js";
 import { toolHandlers, nodeTools } from "./tools/nodeAgentTools.js";
 import { nodeSkills } from "./skills/nodeAgentSkills.js";
-import { agentInstructions } from "./agentInstructions.js";
+import { agentInstructions } from "./instructions/infrastructure_instructions.js";
 
 class NodeDeveloperAgent extends BaseAgent {
 
