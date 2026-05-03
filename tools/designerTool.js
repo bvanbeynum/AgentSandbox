aded4773b3c8d5527d9fafe7413e713515cfc5e3
bvanbeynum <maildrop444@gmail.com>
2026-05-03 10:06:05 -0400

    updated thresholds

--- tools/designerTool.js
+++ tools/designerTool.js
@@ -35,6 +35,18 @@ export const designerToolHandlers = {
 						category: "HARM_CATEGORY_HARASSMENT",
 						threshold: "BLOCK_MEDIUM_AND_ABOVE",
 					},
+					{
+						category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+						threshold: "BLOCK_ONLY_HIGH",
+					},
+					{
+						category: "HARM_CATEGORY_HATE_SPEECH",
+						threshold: "BLOCK_MEDIUM_AND_ABOVE",
+					},
+					{
+						category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+						threshold: "BLOCK_ONLY_HIGH",
+					},
 				],
 			});
 
